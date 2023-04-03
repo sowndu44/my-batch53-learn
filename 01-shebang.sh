77a0a8b0ea152572035198f669b653e386910fc0
sowndu44 <sowndu44@gmail.com>
2023-04-03 09:00:49 +0530

    2nd commit

---
+++ 01-shebang.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#shebang line tells system that intrepreter used for the script is bash/python/shell
+#cannot have more than 1 shebang notations.
+#echo is like printing something -- echo "hello world"
+
+echo "hello sowndarya"
